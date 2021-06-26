84a5a67ce52306ad0767bd52a584a783c89f2d09
Matt Koski <mjkekai@gmail.com>
2021-06-26 10:03:25 -0700

    added json packages and inquirer

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ReadMe_Generator
+# Taco
+
+![GitHub license](https://img.shields.io/badge/license-BSD3-blue.svg)
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Description](#description)
+- [License](#license)
+- [Credits](#credits)
+- [Contributing](#contributing)
+
+## Description 
+
+taco
+
+## Usage
+
+taco
+
+## Contributing
+
+taco
+
+## Installation
+
+try
+
+## Credits
+
+
+
+## License
+
+BSD3
+
--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your application",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Write a brief description of your application: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is this application usage for?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who are the contributors of this projects?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "license?",
+      choices: ["MIT", "BSD3", "APACHE2.0", "GPL3.0"],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How to install?",
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+      },
+  ])
+  .then((answers) => {
+    console.log("answers: ", answers);
+    fs.writeFileSync(
+      path.join(__dirname, "README.md"),
+      generateMarkdown(answers)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
